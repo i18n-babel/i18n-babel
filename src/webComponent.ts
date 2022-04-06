ee85aa26cd2e228405a05faaedec95b8afef81a9
i18n-babel <info@i18n-babel.com>
2022-04-06 12:13:40 +0200

    fix Failed to construct 'CustomElement' the result must not have attributes

--- src/webComponent.ts
+++ src/webComponent.ts
@@ -2,9 +2,12 @@ import { TManager } from './tManager';
 
 export class I18nBabelWebcomponent extends HTMLElement {
     static dataAttribute: string;
+    private isAttached = false;
 
-    constructor() {
-        super();
-        TManager.attach(this, this.getAttribute(I18nBabelWebcomponent.dataAttribute));
+    connectedCallback() {
+        if (!this.isAttached) {
+            this.isAttached = true;
+            TManager.attach(this, this.getAttribute(I18nBabelWebcomponent.dataAttribute));
+        }
     }
 }
